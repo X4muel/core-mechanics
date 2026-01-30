6e7b24b2119bc79fb68f2c67e5fbda489961beda
X4muel <samu.eae1210@gmail.com>
2026-01-30 15:51:55 -0300

    feat:Simple breakable blocks

--- Character/bullet.gd
+++ Character/bullet.gd
@@ -11,3 +11,17 @@ func _physics_process(_delta: float) -> void:
 		velocity = Vector2.ZERO
 	
 	move_and_slide()
+	
+	var collision = move_and_collide(direction * speed * _delta)
+	if collision:
+		var collider = collision.get_collider()
+		
+		if collider is TileMapLayer: # Ou TileMap dependendo da versão
+			var tile_pos = collider.local_to_map(collision.get_position() - collision.get_normal())
+			var data = collider.get_cell_tile_data(tile_pos)
+			
+			if data and data.get_custom_data("can_break"):
+				collider.erase_cell(tile_pos) # O bloco some!
+				# Opcional: Instancie partículas na posição global do tile
+		
+		queue_free()
